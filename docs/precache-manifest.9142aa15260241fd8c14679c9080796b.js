a4b8c0d674d554c48a5dd443a032b1806ff78751
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-11-14 19:58:31 -0500

    added docs folder for github pages

---
+++ docs/precache-manifest.9142aa15260241fd8c14679c9080796b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8d6554c5f51035760590e99fce2ee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2217a848514083dc472",
+    "url": "/static/css/main.5d592dd2.chunk.css"
+  },
+  {
+    "revision": "4ee023134575dcc6ab90",
+    "url": "/static/js/2.7631f5d8.chunk.js"
+  },
+  {
+    "revision": "d2217a848514083dc472",
+    "url": "/static/js/main.8da0140a.chunk.js"
+  },
+  {
+    "revision": "560039494c475cc88b1e",
+    "url": "/static/js/runtime-main.943a1612.js"
+  },
+  {
+    "revision": "b4f844e4d7328fb6998c1379c52f6997",
+    "url": "/static/media/Background.b4f844e4.jpg"
+  }
+]);
