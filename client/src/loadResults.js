00ba31141e625060fa26e5b593a655f44ea62cb5
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-02-11 19:16:39 -0500

    started pulling info from flight response

--- client/src/loadResults.js
+++ client/src/loadResults.js
@@ -36,6 +36,21 @@ export const loadResults = async data => {
     return json;
   };
 
+  const flightPoll = async searchURL => {
+    const response = await fetch(
+      `https://tripadvisor1.p.rapidapi.com/flights/poll?currency=USD&ns=NON_STOP%252CONE_STOP&so=PRICE&sid=${searchURL}`,
+      {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
+          'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
+        }
+      }
+    );
+    const json = await response.json();
+    return json;
+  };
+
   await airportSearch(originCity).then(
     res => (flightData.originCityAirport = res[0].code)
   );
@@ -45,7 +60,24 @@ export const loadResults = async data => {
   await flightSearch(
     flightData.originCityAirport,
     flightData.destCityAirport
-  ).then(res => console.log(res));
-
-  console.log(flightData);
+  ).then(res => {
+    flightPoll(res.search_params.sid).then(res2 => {
+      let flight = res2.itineraries[0];
+      let ap1code = flight.f[0].l[0].da;
+      let legs = [
+        {
+          ap1: {
+            code: ap1code,
+            name: res2.airports.forEach(
+              airport => airport.c == ap1code && airport.n
+            )
+          }
+        }
+      ];
+      res2.airports.forEach(airport => console.log(airport));
+      console.log(res2);
+      console.log(flight);
+      console.log(legs);
+    });
+  });
 };
