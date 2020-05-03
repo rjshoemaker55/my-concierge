db3a13cd4a979b148e95aa5d06bbc1f705060048
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-05-02 20:43:13 -0400

    typo[

--- client/src/loadResults.js
+++ client/src/loadResults.js
@@ -5,20 +5,21 @@ export const loadResults = async (data) => {
     https://tripadvisor1.p.rapidapi.com/locations/auto-complete?lang=en_US&units=mi&query=${destCity}
   `;
 
-  const getLocationId = await locationQueryString => {
-  fetch(locationQueryString, {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
-      'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c',
-    },
-  }).then((res) => console.log(res.json().data.result_object.location_id));
+  const getLocationId = (locationQueryString) => {
+    fetch(locationQueryString, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
+        'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c',
+      },
+    }).then((res) => console.log(res.json().data.result_object.location_id));
 
-  // let hotelList = await fetch(hotelQueryString, {
-  //   method: 'GET',
-  //   headers: {
-  //     'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
-  //     'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c',
-  //   },
-  // }).then((res) => console.log(res.json()));
-}};
+    // let hotelList = await fetch(hotelQueryString, {
+    //   method: 'GET',
+    //   headers: {
+    //     'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
+    //     'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c',
+    //   },
+    // }).then((res) => console.log(res.json()));
+  };
+};
