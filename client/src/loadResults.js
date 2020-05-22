f459a5e98fffdc1bedeb24fe175cf67a4616f604
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-05-21 21:03:09 -0400

    returning locationn id from loadresults

--- client/src/loadResults.js
+++ client/src/loadResults.js
@@ -5,14 +5,16 @@ export const loadResults = async (data) => {
     https://tripadvisor1.p.rapidapi.com/locations/auto-complete?lang=en_US&units=mi&query=${destCity}
   `;
 
-  const getLocationId = (locationQueryString) => {
-    fetch(locationQueryString, {
+  const getLocationId = async (locationQueryString) => {
+    const locationQuery = await fetch(locationQueryString, {
       method: 'GET',
       headers: {
         'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
         'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c',
       },
-    }).then((res) => console.log(res.json().data.result_object.location_id));
+    });
+    const locationResponse = await locationQuery.json();
+    return locationResponse.data[0].result_object.location_id;
 
     // let hotelList = await fetch(hotelQueryString, {
     //   method: 'GET',
@@ -22,4 +24,6 @@ export const loadResults = async (data) => {
     //   },
     // }).then((res) => console.log(res.json()));
   };
+
+  getLocationId(locationQueryString).then((res) => console.log(res));
 };
