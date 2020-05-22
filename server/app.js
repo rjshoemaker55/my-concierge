9449ee4c4e3bfff8566a3c02f23ea7e9b4c36f4c
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-05-22 18:51:10 -0400

    started route for hotel list

--- server/app.js
+++ server/app.js
@@ -6,6 +6,14 @@ const port = 4000;
 
 app.use(cors());
 
+let options = {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
+    'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c',
+  },
+};
+
 app.get('/', (req, res) => res.send('Hello world'));
 
 app.get('/locationid/:cityName', async (req, res) => {
@@ -18,15 +26,13 @@ app.get('/locationid/:cityName', async (req, res) => {
   `;
 
   const locationQuery = await fetch(locationQueryString, {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
-      'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c',
-    },
+    options,
   });
   const locationResponse = await locationQuery.json();
   console.log(locationResponse.data[0].result_object.location_id);
   res.send(locationResponse.data[0].result_object.location_id);
 });
+
+app.get('/hotellist/:locationid/:checkin/:nights');
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
