ff7ea4112d5be7e61efaf1fc763dfb33ae6bbfd6
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-05-26 20:11:52 -0400

    added styled components and started styling hotel list

--- client/src/components/HotelDisplay/index.js
+++ client/src/components/HotelDisplay/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import styled from 'styled-components';
 import './styles.css';
 
 const HotelDisplay = (props) => {
+  const largePhotoUrl = props.largePhoto;
+
+  const HotelWrapper = styled.div`
+    background-image: url(${largePhotoUrl});
+    background-size: cover;
+    width: 100%;
+    height: 400px;
+    background-attachment: fixed;
+  `;
+
+  const HotelHeadingWrapper = styled.div`
+    background-color: rgba(0, 0, 0, 0.5);
+  `;
+
+  const HotelMainHeading = styled.h1`
+    color: white;
+    font-size: 45px;
+    font-weight: bold;
+  `;
+
+  const HotelSecHeading = styled.h2`
+    color: black;
+    font-size: 23px;
+    font-weight: bold;
+  `;
+
   return (
-    <div className='hotelDisplay'>
-      <h1 className='main-heading'>{props.name}</h1>
-      <h2 className='sec-heading'>{props.locationString}</h2>
-      <img src={props.smallPhoto} />
-    </div>
+    <HotelWrapper>
+      <HotelHeadingWrapper>
+        <HotelMainHeading>{props.name}</HotelMainHeading>
+        <HotelSecHeading>{props.locationString}</HotelSecHeading>
+      </HotelHeadingWrapper>
+    </HotelWrapper>
   );
 };
 
