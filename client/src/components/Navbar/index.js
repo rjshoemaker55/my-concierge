54b204c74cd3ea74a5c103248152e5af2a68fcea
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-06-01 11:33:44 -0400

    some navbar and landing styling

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div id='navbar-wrapper'>
       <h1 id='navbar-title'>my concierge</h1>
       <div id='navbar-form'>
-        <h2 id='navbar-form-label'>search:</h2>
+        {/* <h2 id='navbar-form-label'>search:</h2> */}
         <input className='navbar-inputs' placeholder='city' />
         <input className='navbar-inputs' placeholder='arrival date' />
         <input className='navbar-inputs' placeholder='nights' />
