09916d4fc435f06d836233d12c8be60dc998da34
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-06-01 09:56:24 -0400

    removed original landing page

--- client/src/components/HotelDisplay/index.js
+++ client/src/components/HotelDisplay/index.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import styled from 'styled-components';
 import './styles.css';
+import backgroundImage from '../../images/Background.jpg';
 
 const HotelDisplay = (props) => {
   const [showInfo, setShowInfo] = useState('none');
   const largePhotoUrl = props.largePhoto;
 
   const HotelWrapper = styled.div`
-    background-image: url(${largePhotoUrl});
+    background-image: url(${props.originalPhoto});
     background-size: cover;
     width: 100%;
     height: 400px;
@@ -35,10 +36,15 @@ const HotelDisplay = (props) => {
   const HotelInfoWrapper = styled.div`
     display: ${showInfo};
     position: absolute;
-    right: 0;
+    right: 10px;
     width: 45%;
-    height: 100%;
+    height: 73%;
     background: rgba(0, 0, 0, 0.85);
+    animation: fadeIn ease 1s;
+    -webkit-animation: fadeIn ease 0.45s;
+    -moz-animation: fadeIn ease 0.45s;
+    -o-animation: fadeIn ease 0.45s;
+    -ms-animation: fadeIn ease 0.45s;
   `;
 
   return (
@@ -49,7 +55,12 @@ const HotelDisplay = (props) => {
         <HotelMainHeading>{props.name.toLowerCase()}</HotelMainHeading>
         <HotelSecHeading>{props.locationString.toLowerCase()}</HotelSecHeading>
       </HotelHeadingWrapper>
-      <HotelInfoWrapper>Info</HotelInfoWrapper>
+      <HotelInfoWrapper>
+        <div className='hotel-info-header'>information</div>
+        <div className='hotel-info-body'>
+          <div className='hotel-ranking-text'>"{props.rankingText}"</div>
+        </div>
+      </HotelInfoWrapper>
     </HotelWrapper>
   );
 };
--- client/src/screens/Landing/index.js
+++ client/src/screens/Landing/index.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { BrowserHistory } from 'react-router';
 import { Modal } from 'react-bootstrap';
+import Navbar from '../../components/Navbar';
 import { loadResults } from '../../loadResults';
 import Results from '../Results/index';
-import FormQuestion from '../../components/FormQuestion';
 import './styles.css';
 
 const Landing = () => {
@@ -15,75 +15,6 @@ const Landing = () => {
   const [numberNights, setNumberNights] = useState('');
   const [questionNumber, setQuestionNumber] = useState(0);
   const [showModal, setShowModal] = useState(false);
-
-  const questionList = [
-    {
-      mainHeader: (
-        <span>
-          <span className='small-sec'>welcome to</span> My Concierge.
-        </span>
-      ),
-      secHeader: `What's your first name?`,
-      name: 'firstName',
-      value: firstName,
-    },
-    {
-      mainHeader: `Hello ${firstName}!`,
-      secHeader: `Where are you headed? (city name)`,
-      name: 'destCity',
-      value: destCity,
-    },
-    {
-      mainHeader: (
-        <span>
-          Ahh {destCity}, <span className='small-sec'>great choice!</span>
-        </span>
-      ),
-      secHeader: `When will you be arriving (YYYY-MM-DD)?`,
-      name: 'arriveDate',
-      value: arriveDate,
-    },
-    {
-      mainHeader: 'Last step!',
-      secHeader: `Alright, and how many nights will you be staying?`,
-      name: 'numberNights',
-      value: numberNights,
-    },
-  ];
-
-  // Sets current question to the question data with index of question number in state
-  let currentQuestion = questionList[questionNumber];
-
-  // Sets data in state based on input changes
-  const handleChange = (e) => {
-    switch (e.target.name) {
-      case 'firstName':
-        setFirstName(e.target.value);
-        break;
-      case 'destCity':
-        setDestCity(e.target.value);
-        break;
-      case 'arriveDate':
-        setArriveDate(e.target.value);
-        break;
-      case 'numberNights':
-        setNumberNights(e.target.value);
-        break;
-    }
-  };
-
-  // Initiates handleFinish if all questions have been answereed
-  // If not, icreases question number by 1, and sets the currentQuestion so the displayed question changes
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (questionNumber === 3) {
-      return handleFinish();
-    }
-
-    await setQuestionNumber(questionNumber + 1);
-    currentQuestion = questionList[questionNumber];
-  };
 
   // Calls loadResults function with all input data, then redirects to results page and sends the response as props
   const handleFinish = async () => {
@@ -102,21 +33,22 @@ const Landing = () => {
   };
 
   return (
-    <div className='landing-wrapper'>
-      <FormQuestion
-        mainHeader={currentQuestion.mainHeader}
-        secHeader={currentQuestion.secHeader}
-        name={currentQuestion.name}
-        onChange={handleChange}
-        onSubmit={handleSubmit}
-        value={currentQuestion.value}
-      />
-      <Modal show={showModal}>
-        <Modal.Header>
-          <Modal.Title>Please wait while we load your results...</Modal.Title>
-        </Modal.Header>
-      </Modal>
-    </div>
+    <>
+      <Navbar />
+
+      <div className='landing-wrapper'>
+        <div className='landing-header-wrapper'>
+          <div className='landing-small-header'>welcome to</div>
+          <div className='landing-header'>my concierge</div>
+        </div>
+
+        <Modal show={showModal}>
+          <Modal.Header>
+            <Modal.Title>Please wait while we load your results...</Modal.Title>
+          </Modal.Header>
+        </Modal>
+      </div>
+    </>
   );
 };
 
