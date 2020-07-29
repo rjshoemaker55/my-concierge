4551c3abcf09d7e60ddfafc95128328901a668fd
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-07-28 22:18:43 -0400

    stillt ypring

---
+++ build/precache-manifest.2af26466f3e63178e6f0beb1ed0ab2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2b1b25d25a712c20ccb1282d665b29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4bfa463ab7026c0755",
+    "url": "/static/css/main.6a5abe38.chunk.css"
+  },
+  {
+    "revision": "2a07b4ce4ee0a29bd386",
+    "url": "/static/js/2.037311ab.chunk.js"
+  },
+  {
+    "revision": "fb4bfa463ab7026c0755",
+    "url": "/static/js/main.e7dfe928.chunk.js"
+  },
+  {
+    "revision": "560039494c475cc88b1e",
+    "url": "/static/js/runtime-main.943a1612.js"
+  },
+  {
+    "revision": "b4f844e4d7328fb6998c1379c52f6997",
+    "url": "/static/media/Background.b4f844e4.jpg"
+  }
+]);
