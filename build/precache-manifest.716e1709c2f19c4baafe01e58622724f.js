5ef91d50f8241658e0605881ce3b6b953491e27d
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-07-29 20:13:55 -0400

    trying to move build out of clinet

---
+++ build/precache-manifest.716e1709c2f19c4baafe01e58622724f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f179970b6ced14fefcb673c237de1190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "996f30ceacdf636359c1",
+    "url": "/static/css/main.6a5abe38.chunk.css"
+  },
+  {
+    "revision": "2a07b4ce4ee0a29bd386",
+    "url": "/static/js/2.037311ab.chunk.js"
+  },
+  {
+    "revision": "996f30ceacdf636359c1",
+    "url": "/static/js/main.1e4fff05.chunk.js"
+  },
+  {
+    "revision": "560039494c475cc88b1e",
+    "url": "/static/js/runtime-main.943a1612.js"
+  },
+  {
+    "revision": "b4f844e4d7328fb6998c1379c52f6997",
+    "url": "/static/media/Background.b4f844e4.jpg"
+  }
+]);
